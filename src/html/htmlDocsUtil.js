16fdf8e7bf27f687b8bd117efff0f1cd8ae55aa9
anderslatif <anderslatif@users.noreply.github.com>
2024-11-07 18:57:38 +0100

    Updated the CSS look of the generated HTML docs page

--- src/html/htmlDocsUtil.js
+++ src/html/htmlDocsUtil.js
@@ -7,26 +7,56 @@ export function getHTMLDocument(tables, database) {
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <style>
-            h1 {
-                text-align: center;
+            body {
+                margin: 5vw;
+                background: #fafafa;
+                font-family: "Roboto", "Helvetica", "Arial", sans-serif;
             }
             
-            table, th, td {
-                border: 1px solid black;
+            h1, h2 {
+                font-family: Titillium Web, sans-serif;
+                color: #3b4151;
             }
-    
-            main {
-                min-height: calc(100vh - 40px);
-                margin: 0 4em 0 4em;
+            
+            h1 {
+                font-size: 36px;
             }
-    
+            
+            h2 {
+                font-size: 24px;
+                margin-bottom: 10px;
+            }
+            
             table {
                 width: 100%;
+                margin-bottom: 15px;
+                border-collapse: collapse;
+                border-radius: 4px;
+                box-shadow: 0 2px 2px 0 rgba(0, 0, 0, .14), 0 3px 1px -2px rgba(0, 0, 0, .2), 0 1px 5px 0 rgba(0, 0, 0, .12);
+                background: #ffffff;
             }
-    
-            footer {
-                color: grey;
-                font-size: 0.8em;
+            
+            th, td {
+                font-size: 14px;
+                padding: 10px;
+                text-align: left;
+                font-family: Source Code Pro, monospace;
+                font-weight: 600;
+                color: #3b4151;
+                border-bottom: 1px solid rgba(59, 65, 81, .2);
+            }
+            
+            th {
+                font-weight: 700;
+                background: hsla(0, 0%, 100%, .8);
+            }
+            
+            tr:nth-child(even) {
+                background-color: rgba(97, 175, 254, .1);
+            }
+            
+            tbody tr:hover {
+                background: rgba(0, 0, 0, .02);
             }
         </style>
         <title>%%DATABASE_NAME%% - Docs</title>
