e9b71277a4dc5fb3523e42fd9fe90a7d569062a6
anderslatif <anderslatif@users.noreply.github.com>
2025-01-07 11:53:05 +0100

    Updated the .env variables for the knex test

--- tests/testUtil/testRunCLI.js
+++ tests/testUtil/testRunCLI.js
@@ -2,15 +2,11 @@ import { execSync } from 'child_process';
 import { resolve } from 'path';
 
 export function testRunCLI() {
-	try {
-		const cliPath = resolve('src', 'cli.js');
-        console.log('cliPath:', cliPath);
-		execSync(`node ${cliPath}`, {
-			env: {
-			...process.env,
-			}
-		});
-	} catch (error) {
-		throw error;
-	}
+	const cliPath = resolve('src', 'cli.js');
+	console.log('cliPath:', cliPath);
+	execSync(`node ${cliPath}`, {
+		env: {
+		...process.env,
+		}
+	});
 }
