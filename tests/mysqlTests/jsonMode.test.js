4134a88f05f4fd1f9d59d8674bfe091154544ee8
anderslatif <anderslatif@users.noreply.github.com>
2025-01-08 05:07:17 +0100

    Testing Objection tests only

--- tests/mysqlTests/jsonMode.test.js
+++ tests/mysqlTests/jsonMode.test.js
@@ -1,38 +1,38 @@
-import { expect } from 'chai';
+// import { expect } from 'chai';
 
-import { envMySQL, envMySQLCredentials, envMySQLPort, envJSONMode, envAllKeys, envAllTables  } from '../testUtil/setupEnvironment.js';
+// import { envMySQL, envMySQLCredentials, envMySQLPort, envJSONMode, envAllKeys, envAllTables  } from '../testUtil/setupEnvironment.js';
 
-import { testRunCLI } from '../testUtil/testRunCLI.js';
-import fs from 'fs';
+// import { testRunCLI } from '../testUtil/testRunCLI.js';
+// import fs from 'fs';
 
 
-describe('Test JSON Docs creation for MySQL', () => {
-	before('Run the CLI', () => {
-		envMySQL();
-     envMySQLCredentials();
-		envMySQLPort();
-		envJSONMode();
+// describe('Test JSON Docs creation for MySQL', () => {
+// 	before('Run the CLI', () => {
+// 		envMySQL();
+//      envMySQLCredentials();
+// 		envMySQLPort();
+// 		envJSONMode();
 
-		envAllKeys();
-		envAllTables();
+// 		envAllKeys();
+// 		envAllTables();
 
-		testRunCLI();
-	});
+// 		testRunCLI();
+// 	});
 
-	it('should create the JSON file', () => {
-		const fileExists = fs.existsSync('sakila.json');
+// 	it('should create the JSON file', () => {
+// 		const fileExists = fs.existsSync('sakila.json');
 
-		expect(fileExists).to.be.true;
-	});
+// 		expect(fileExists).to.be.true;
+// 	});
 
-	it('should have the correct number of tables', () => {
-		const fileContent = fs.readFileSync('sakila.json', 'utf8');
-		const sakilaJSON = JSON.parse(fileContent);
+// 	it('should have the correct number of tables', () => {
+// 		const fileContent = fs.readFileSync('sakila.json', 'utf8');
+// 		const sakilaJSON = JSON.parse(fileContent);
 		
-		const tableCount = sakilaJSON.schema.length;
+// 		const tableCount = sakilaJSON.schema.length;
 
-		const sakilaTableCount = 23;
+// 		const sakilaTableCount = 23;
 
-		expect(tableCount).to.equal(sakilaTableCount);
-	});
-});
+// 		expect(tableCount).to.equal(sakilaTableCount);
+// 	});
+// });
